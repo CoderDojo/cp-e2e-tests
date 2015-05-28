572795177d7cd15b27acaca846a14eacd9dc62a3
Kamil Mech <kamil.mech@nearform.com>
2015-05-28 14:46:08 +0100

    fix phantom implementation

--- test/smoke.js
+++ test/smoke.js
@@ -12,20 +12,27 @@ console.log('port: ' + port);
 
 var selectors = JSON.parse(fs.readFileSync(__dirname + '/selectors.json', 'utf8'));
 
+function click(browser, selector){
+  browser
+    .waitForElementVisible(selector, defaultTimeout)
+    .click(selector)
+    .pause(750);
+}
+
 function fill(browser, selector, value, desc){
   browser
-    .verify.visible(selector, desc)
-    .setValue(selector, value)
+    .waitForElementVisible(selector, defaultTimeout, desc)
+    .setValue(selector, value);
 }
 
 function logout(browser, selectors){
+  browser.waitForElementVisible('header', defaultTimeout);
+  click(browser, selectors.logout);
   browser
-    .waitForElementPresent(selectors.logout, defaultTimeout)
-  browser
-    .click(selectors.logout)
-    .waitForElementPresent('body', defaultTimeout)
-    .waitForElementPresent(selectors.register, defaultTimeout)
-    .waitForElementPresent(selectors.login, defaultTimeout)
+    .waitForElementVisible('header', defaultTimeout)
+    .waitForElementVisible('body', defaultTimeout)
+    .waitForElementVisible(selectors.register, defaultTimeout)
+    .waitForElementVisible(selectors.login, defaultTimeout);
 }
 
 module.exports = {
@@ -34,21 +41,17 @@ module.exports = {
     // init
     browser
       .url(host + ':' + port)
-      .waitForElementPresent('body', defaultTimeout)
+      .waitForElementVisible('body', defaultTimeout);
       // .source(function (result){
       //     console.log('src: ' + result.value);
-      // })
+      // });
 
     // search dojo
-    fill(browser, 'input[ng-model="search.dojo"]', 'dublin', 'Dojo search field')
-    browser
-      .click('button[type="submit"]')
-      .waitForElementPresent('body', defaultTimeout)
-      .waitForElementPresent(selectors.dojoclick, defaultTimeout)
-      .verify.visible(selectors.dojoclick, 'Dojos to click')
-      .click(selectors.dojoclick)
-      .waitForElementPresent(selectors.div.main, defaultTimeout)
-      .assert.containsText(selectors.div.main, 'Dublin');
+    fill(browser, 'input[ng-model="search.dojo"]', 'dublin', 'Dojo search field');
+    click(browser,'button[type="submit"]');
+    browser.waitForElementVisible('body', defaultTimeout);
+    click(browser, selectors.dojoclick);
+    browser.waitForElementVisible(selectors.div.ng, defaultTimeout);
 
     // register
     var payload = {
@@ -57,35 +60,31 @@ module.exports = {
       password: 'Password1'
     };
 
-    browser
-      .click(selectors.register)
-      .waitForElementPresent('body', defaultTimeout);
+    click(browser, selectors.register);
+    browser.waitForElementVisible('body', defaultTimeout);
     fill(browser, selectors.reg.name, payload.name, 'Register name field');
     fill(browser, selectors.reg.email, payload.email, 'Register email field');
     fill(browser, selectors.reg.pass, payload.password, 'Register pass field');
     fill(browser, selectors.reg.passconfirm, payload.password, 'Register pass confirm field');
-    browser
-      .click('button[type="submit"]')
-      .verify.visible('label[class="control-label has-error validationMessage"]', 'Err msg visible')
-      .click('label[for="termsAndConditionsCheckbox"]') // for some reason only clicking the label checks the checkbox
-      .click('button[type="submit"]')
+    click(browser, 'button[type="submit"]');
+    browser.waitForElementVisible('label[class="control-label has-error validationMessage"]', defaultTimeout, 'Err msg visible');
+    click(browser, 'label[for="termsAndConditionsCheckbox"]'); // for some reason only clicking the label checks the checkbox
+    click(browser, 'button[type="submit"]');
 
-    logout(browser, selectors)
+    logout(browser, selectors);
 
     // safe option: u: manager@example.com, p: test
     // login
-    browser.click(selectors.login);
+    click(browser, selectors.login);
     fill(browser, selectors.log.name, payload.email, 'Login name field');
     fill(browser, selectors.log.pass, payload.password, 'Login pass field');
-    browser
-      .click("input[type='submit']")
-      .waitForElementPresent('body', defaultTimeout);
+    click(browser, "input[type='submit']");
+    browser.waitForElementVisible('body', defaultTimeout);
 
     logout(browser, selectors);
 
     // end
     browser
-      // .pause(defaultTimeout)
       .end();
   }
 };
