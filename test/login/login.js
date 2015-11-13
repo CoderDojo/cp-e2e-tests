c33315e09bc4b1c224cb381fd4b9910b97ce95e2
Mihai Dima <mihaizn@gmail.com>
2015-11-13 15:12:48 +0200

    multiple clicks on user, navigation back and forth

--- test/login/login.js
+++ test/login/login.js
@@ -12,23 +12,37 @@ module.exports = {
     home.setValue('@email', browser.globals.mentor1.login)
     .setValue('@password', browser.globals.mentor1.password)
     .submit()
-    // .expect.element('@userName').text.to.equal(browser.globals.mentor1.name);
+    .expect.element('@userName').text.to.equal(browser.globals.mentor1.name);
+  },
 
-    // crash test simulation
+  'Multiple Clicks On User Name': function(browser) {
     var myDojosUrl = browser.launch_url + '/dashboard/my-dojos';
-    for (var i=0; i<30; i++) {
+    var home = browser.page.login();
+
+    home.click('@userName')
+    home.click('@userName')
+    home.click('@userName')
+    home.api.pause(1000);
+    home.navigate(myDojosUrl)
+    .waitForElementVisible('@eventsView', 1000);
+  },
+
+  'Show Dojos, Show Events, Dojo View Back and Forth': function(browser) {
+    var myDojosUrl = browser.launch_url + '/dashboard/my-dojos';
+    var home = browser.page.login();
+    for (var i=0; i<8; i++) {
       home.navigate(myDojosUrl)
-      .waitForElementVisible('@eventsView', 2000)
+      .waitForElementVisible('@eventsView', 1000)
       .click('@eventsView')
-      .api.pause(2000);
+      .api.pause(1000);
 
       home.navigate(myDojosUrl)
-      .waitForElementVisible('@listingView', 2000)
+      .waitForElementVisible('@listingView', 1000)
       .click('@listingView')
-      .api.pause(2000);
+      .api.pause(1000);
 
       home.navigate(myDojosUrl)
-      .waitForElementVisible('@eventsView', 2000);
+      .waitForElementVisible('@eventsView', 1000);
     }
   },
 
