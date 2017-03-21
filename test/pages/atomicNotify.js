3351f6fd6f9ea72b87b577a7c5d719b2f5da38da
Wardormeur <wardormeur@gmail.com>
2017-03-21 09:20:17 +0000

    Add tests for pending users

---
+++ test/pages/atomicNotify.js
@@ -0,0 +1,23 @@
+var Page = require('./page');
+
+var MyDojosPage = Object.create(Page, {
+  /**
+   * define elements
+   */
+  get: {
+    get: function () {
+      var xpath = '//div[contains(@class, "atomic-notify-item") and contains(@class, "atomic-notify-success")]//p';
+      return browser.waitForVisible(xpath)
+      .then(() => {
+        return $(xpath);
+      });
+    }
+  },
+  close: {
+    get: function () {
+      return $('//div[contains(@class, "atomic-notify-item")]//button[@class="close"]');
+    }
+  }
+});
+
+module.exports = MyDojosPage;
